f9e37e1ac592ddf59832399a80158430a7735fa8
Magnus Merkle <magnus.merkle@nmbu.no>
2024-01-20 12:41:56 +0100

    ecp-gloria ipcc concordance

--- _code/compilation/ecp/industry/run_import_and_match.R
+++ _code/compilation/ecp/industry/run_import_and_match.R
@@ -5,8 +5,15 @@ wd<-file.path(here::here(),"_code","compilation","ecp","industry")
 setwd(wd)
 
 ### Import data
-gloriawd<-file.path(wd,"import","gloria")
+gloriawd<-file.path(wd,"1_import","gloria")
 source(file.path(gloriawd,"run_imports.R"))
 
+pl<-"curr_p"
+ecpwd<-file.path(wd,"1_import","ecp")
+source(file.path(ecpwd,"import_ecp.R"))
 
-# MM to continue
+### Datamatch
+ecpmwd<-file.path(wd,"2_match")
+
+## MM hier weiter
+source(file.path(ecpmwd,"match_gloria_ecp.R"))
